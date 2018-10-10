38a9e00712a97022ac6265ecd8f60a1acc43c091
Marc Freiman <marcfb2@hotmail.com>
2018-10-10 10:28:00 -0400

    fit to screen all images mobiel and desktop

--- src/components/nature.js
+++ src/components/nature.js
@@ -4,10 +4,10 @@ class Nature extends Component{
   render(){
     return(
       <div className='nature-img'>
-        <img src={process.env.PUBLIC_URL + '/spider.jpeg'} />
-        <img src={process.env.PUBLIC_URL + '/focas.jpeg'} />
-        <img src={process.env.PUBLIC_URL + '/rusty.jpeg'} />
-        <img src={process.env.PUBLIC_URL + '/bote.jpeg'} />
+        <img style={{width: "100%"}} src={process.env.PUBLIC_URL + '/spider.jpeg'} />
+        <img style={{width: "100%"}} src={process.env.PUBLIC_URL + '/focas.jpeg'} />
+        <img style={{width: "100%"}} src={process.env.PUBLIC_URL + '/rusty.jpeg'} />
+        <img style={{width: "100%"}} src={process.env.PUBLIC_URL + '/bote.jpeg'} />
       </div>
     );
   }
--- src/components/portrait.js
+++ src/components/portrait.js
@@ -4,10 +4,10 @@ class Portrait extends Component{
   render(){
     return(
       <div className='portrait-img'>
-        <img src={process.env.PUBLIC_URL + '/split.jpeg'} />
-        <img src={process.env.PUBLIC_URL + '/viejo.jpeg'} />
-        <img src={process.env.PUBLIC_URL + '/indios.jpeg'} />
-        <img style={{width: "75%"}}src={process.env.PUBLIC_URL + '/katrina.jpeg'} />
+        <img style={{width: "100%"}} src={process.env.PUBLIC_URL + '/split.jpeg'} />
+        <img style={{width: "100%"}} src={process.env.PUBLIC_URL + '/viejo.jpeg'} />
+        <img style={{width: "100%"}} src={process.env.PUBLIC_URL + '/indios.jpeg'} />
+        <img style={{width: "100%"}} src={process.env.PUBLIC_URL + '/katrina.jpeg'} />
       </div>
     )
   }
