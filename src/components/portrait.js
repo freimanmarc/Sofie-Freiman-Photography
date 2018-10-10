48be8a4e37b2d05ed0e8e73670f500db2e619539
Marc Freiman <marcfb2@hotmail.com>
2018-10-10 10:20:38 -0400

    style katrina

--- src/components/portrait.js
+++ src/components/portrait.js
@@ -7,7 +7,7 @@ class Portrait extends Component{
         <img src={process.env.PUBLIC_URL + '/split.jpeg'} />
         <img src={process.env.PUBLIC_URL + '/viejo.jpeg'} />
         <img src={process.env.PUBLIC_URL + '/indios.jpeg'} />
-        <img src={process.env.PUBLIC_URL + '/katrina.jpeg'} />
+        <img style={{width: "75%"}}src={process.env.PUBLIC_URL + '/katrina.jpeg'} />
       </div>
     )
   }
